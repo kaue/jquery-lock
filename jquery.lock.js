7235fd719677717f6a50fcab6c4fbe8bb5120ccd
Kauê Gimenes <kauegimenes@users.noreply.github.com>
2015-05-04 19:20:53 -0300

    Include jquery.lock.js

---
+++ jquery.lock.js
@@ -0,0 +1,52 @@
+//
+// jQuery Lock Plugin
+//
+(function ( $ ) {
+    $.fn.lock = function( options ) {
+		// Keep the reference for later usage
+		var elements = this;
+		
+		// Save the html of the elements		
+		var savedHtmlList = elements.map(function(i, el){
+			return $(el).html();
+		});		
+		
+        // Options
+        var settings = $.extend({
+            // Default options
+            customHandler: null, // Function
+            alertMessage: null 	 // String
+        }, options );
+		
+		// Check for DOM changes
+		$(elements).on('DOMSubtreeModified', function() {
+			var savedHtml = savedHtmlList[elements.index(this)];
+			var updatedHtml = $(this).html();
+			//Check if there is a change in the element HTML
+			if(updatedHtml != savedHtml){
+				// If a customHandler is defined, call it and don`t continue
+				if(settings.customHandler)
+					settings.customHandler(updatedHtml, savedHtml);
+				else
+					$(this).html(savedHtml);
+				// If the alertMessage was set, send the alert
+				if(settings.alertMessage)
+					alert(settings.alertMessage);				
+			}
+		});
+        // Support chaining
+        return elements;
+    };
+}( jQuery ));
+
+
+
+var campoPreco = $('i.price');
+var precoInicial = $(campoPreco).html();
+
+$(campoPreco).on('DOMSubtreeModified', function() {
+         if($(campoPreco).html() != precoInicial){
+                   console.log('Foi detectada uma mudança no preço do produto, de %s foi alterado para %s', precoInicial, $(campoPreco).html());
+                   $(campoPreco).html(precoInicial);
+         }
+});
