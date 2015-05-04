482cb3b94c256a698b9bf4ba7bc69a9041b6ec58
Kauê Gimenes <kauegimenes@users.noreply.github.com>
2015-05-04 19:56:32 -0300

    fix format on jquery.lock.js

--- jquery.lock.js
+++ jquery.lock.js
@@ -3,13 +3,13 @@
 //
 (function ( $ ) {
     $.fn.lock = function( options ) {
-		// Keep the reference for later usage
-		var elements = this;
-		
-		// Save the html of the elements		
-		var savedHtmlList = elements.map(function(i, el){
-			return $(el).html();
-		});		
+	// Keep the reference for later usage
+	var elements = this;
+	
+	// Save the html of the elements		
+	var savedHtmlList = elements.map(function(i, el){
+		return $(el).html();
+	});
 		
         // Options
         var settings = $.extend({
@@ -18,22 +18,22 @@
             alertMessage: null 	 // String
         }, options );
 		
-		// Check for DOM changes
-		$(elements).on('DOMSubtreeModified', function() {
-			var savedHtml = savedHtmlList[elements.index(this)];
-			var updatedHtml = $(this).html();
-			//Check if there is a change in the element HTML
-			if(updatedHtml != savedHtml){
-				// If a customHandler is defined, call it and don`t continue
-				if(settings.customHandler)
-					settings.customHandler(updatedHtml, savedHtml);
-				else
-					$(this).html(savedHtml);
-				// If the alertMessage was set, send the alert
-				if(settings.alertMessage)
-					alert(settings.alertMessage);				
-			}
-		});
+	// Check for DOM changes
+	$(elements).on('DOMSubtreeModified', function() {
+		var savedHtml = savedHtmlList[elements.index(this)];
+		var updatedHtml = $(this).html();
+		//Check if there is a change in the element HTML
+		if(updatedHtml != savedHtml){
+			// If a customHandler is defined, call it and don`t continue
+			if(settings.customHandler)
+				settings.customHandler(updatedHtml, savedHtml);
+			else
+				$(this).html(savedHtml);
+			// If the alertMessage was set, send the alert
+			if(settings.alertMessage)
+				alert(settings.alertMessage);				
+		}
+	});
         // Support chaining
         return elements;
     };
